dfbf4920af700180a92e1193a11f2c52c01dbf07
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-10-23 11:57:40 -0600

    small code typo fix

--- Homework 2/app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DTO/ItemModel.java
+++ Homework 2/app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DTO/ItemModel.java
@@ -1,12 +1,13 @@
 package edu.utep.cs.cs4330.mypricewatcher.DTO;
 
 import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-
+/**
+ * Class will define Item object.
+ */
 public class ItemModel {
     private List<Item> items;
 
@@ -28,17 +29,27 @@ public class ItemModel {
             item.setPriceChage(change);
         }
     }
-
+/**
+ * Method will get the item size.
+ * @return the size.
+ */
     public int getItemSize(){
         return items.size();
     }
-
+/**
+ * Method will update price, for testing.
+ * @param index - the item number.
+ * @param newPrice - the new price.
+ */
     public void updatePrice(int index, double newPrice){
         Item item = items.get(index);
         item.setCurrentPrice(newPrice);
         Log.d("TESTING", "Item name selected: " + item.getCurrentPrice());
     }
-
+/**
+ * Method to remove item.
+ * @param item - the item to be removed.
+ */
     public void removeItem(Item item)
     {
         for(int i = 0; i < items.size(); i++){
@@ -47,6 +58,11 @@ public class ItemModel {
             }
         }
     }
+    /**
+     * Getter for the item.
+     * @param index - the item number.
+     * @return the item.
+     */
     public Item getItem(int index){
         return items.get(index);
     }
