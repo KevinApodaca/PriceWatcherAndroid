926e44a8adbb6e0b62c03284d645679bb94e18fc
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-10-23 11:59:11 -0600

    cleanup

--- Homework 2/app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DTO/PriceFinder.java
+++ Homework 2/app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DTO/PriceFinder.java
@@ -2,15 +2,20 @@ package edu.utep.cs.cs4330.mypricewatcher.DTO;
 
 import java.util.Random;
 
+/**
+ * Class will find the price of the item.
+ */
 public class PriceFinder {
-
+//default constructor.
     PriceFinder(){
     }
-
+/**
+ * Method will generate random price.
+ * @return new price.
+ */
     public double createRandom() {
         double min = 250;
         double max = 400;
-
         Random random = new Random();
         double holder = min + (max - min) * random.nextDouble();
         return Math.round(holder * 100.00) / 100.0;
