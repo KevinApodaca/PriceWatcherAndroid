db9ff77cb7e1d94412ccaea94a2b2353fec9952a
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-11-12 16:40:55 -0700

    finalized documentation

--- Homework 3/app/src/main/java/cs4330/cs/utep/edu/ЕditDialog.java
+++ Homework 3/app/src/main/java/cs4330/cs/utep/edu/ЕditDialog.java
@@ -1,3 +1,39 @@
+/**
+ * CS 4330 Homework 3
+ * @author Kevin Apodaca, Wan Koo
+ * @version 3.0
+ * @since 11/10/19
+ * In this assignment, you are to extend your HW2 code and create the
+ultimate version of the My Price Watcher app that supports network and
+data persistence. Your app shall meet all the relevant requirements
+from the previous homework assignments (HW1 and HW2) as well as the
+following new ones.
+
+R1. The app shall find the price of a watched item from the item's Web
+    page. Remember that the URL of an item is provided by the user
+    when the item is added to the watch list.
+    
+    a. Your app shall inform the user if the price of an item can't be
+       found (e.g., malformed or non-existing URL).
+
+    b. Your app shall support item pages from the following websites:
+       - Home Depot (www.homedepot.com) and
+       - Lowe's (www.lowes.com) or at least two online stores of your 
+         choice
+
+R2. The app shall persist watched items. Use a SQLite database to
+    store watched items. For bonus points, consider using a
+    CursorAdapter to provide data to the UI (a ListView).
+
+R3. The app shall be aware of the current network status: on/off. If
+    Wifi is off, it shall inform the user and direct to the built-in
+    Network (Wifi) Setting app to enable it.
+
+R4. You should separate network and database operations into separate
+    modules (or classes) to decouple them from the rest of the code.
+
+R5. Use the following configuration for your project.
+ */
 package cs4330.cs.utep.edu;
 
 import android.annotation.SuppressLint;
@@ -14,6 +50,9 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/**
+ * Class handles the popup for when a user wants to edit a document using fragments. It allows for removal or addition of information.
+ */
 @SuppressLint("ValidFragment")
 public class ЕditDialog extends DialogFragment {
 
@@ -28,6 +67,9 @@ public class ЕditDialog extends DialogFragment {
         this.activityId = id;
     }
 
+    /**
+     * Method creates the dialog for editing an item.
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         super.onCreateDialog(savedInstanceState);
@@ -48,8 +90,10 @@ public class ЕditDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.editDialogTitle);
 
+        /**
+         * Allowing the user to save changes or to cancel and return to the previous screen.
+         */
         if (this.activityId == 1) {
-
             builder.setView(DialogView)
                     .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
@@ -64,7 +108,8 @@ public class ЕditDialog extends DialogFragment {
                             dialog.dismiss();
                         }
                     });
-        }else{
+        }
+        else{
             builder.setView(DialogView)
                     .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
