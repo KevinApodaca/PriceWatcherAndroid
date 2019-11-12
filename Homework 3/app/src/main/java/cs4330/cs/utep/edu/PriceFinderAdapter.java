f6898222f373716116eeef3c6d93c59a086dc5ea
Kevin Apodaca <kevin.apodaca7@gmail.com>
2019-11-12 16:34:45 -0700

    added documentation for price finder file

--- Homework 3/app/src/main/java/cs4330/cs/utep/edu/PriceFinderAdapter.java
+++ Homework 3/app/src/main/java/cs4330/cs/utep/edu/PriceFinderAdapter.java
@@ -1,5 +1,43 @@
+/**
+ * CS 4330 Homework 3
+ * @author Kevin Apodaca, Wan Koo
+ * @version 3.0
+ * @since 11/10/19
+ * In this assignment, you are to extend your HW2 code and create the
+ultimate version of the My Price Watcher app that supports network and
+data persistence. Your app shall meet all the relevant requirements
+from the previous homework assignments (HW1 and HW2) as well as the
+following new ones.
+
+R1. The app shall find the price of a watched item from the item's Web
+    page. Remember that the URL of an item is provided by the user
+    when the item is added to the watch list.
+    
+    a. Your app shall inform the user if the price of an item can't be
+       found (e.g., malformed or non-existing URL).
+
+    b. Your app shall support item pages from the following websites:
+       - Home Depot (www.homedepot.com) and
+       - Lowe's (www.lowes.com) or at least two online stores of your 
+         choice
+
+R2. The app shall persist watched items. Use a SQLite database to
+    store watched items. For bonus points, consider using a
+    CursorAdapter to provide data to the UI (a ListView).
+
+R3. The app shall be aware of the current network status: on/off. If
+    Wifi is off, it shall inform the user and direct to the built-in
+    Network (Wifi) Setting app to enable it.
+
+R4. You should separate network and database operations into separate
+    modules (or classes) to decouple them from the rest of the code.
+
+R5. Use the following configuration for your project.
+ */
+
 package cs4330.cs.utep.edu;
 
+//imports
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
@@ -28,6 +66,9 @@ import cs4330.cs.utep.edu.models.Item;
 import cs4330.cs.utep.edu.models.ItemManager;
 import cs4330.cs.utep.edu.models.PriceFinder;
 
+/**
+ * Class will handle the price finder for the items.
+ */
 public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
 
     private Context context;
@@ -44,17 +85,25 @@ public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
         this.tmpItems = new ArrayList<PriceFinder>(items);
         this.suggestions = new ArrayList<PriceFinder>(items);
     }
+    /**
+     * Method adds item to list.
+     */
 
     public void addItem(PriceFinder pf) {
         this.tmpItems.add(pf);
     }
+    /**
+     * Allowing user to edit the item.
+     */
 
     public void editItem(int position, String name, String url) {
         PriceFinder tmp = this.tmpItems.get(position);
         tmp.setName(name);
         tmp.setLink(url);
     }
-
+    /**
+     * User can remove the item.
+     */
     public void removeItem(Integer position) {
         PriceFinder tmp = this.tmpItems.get(position);
         this.tmpItems.remove(tmp);
@@ -109,9 +158,9 @@ public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
     private Filter PriceFinderFilter = new Filter() {
 
         /**
-         * Method will do a search of the items (PriceFinder object name) and add them into a
+         * Method will be used to do a search of the items (PriceFinder object name) and add them into a
          * temporal arraylist that will only contain objects that are related to users serach
-         * @param constraint
+         * @param constraint - the item constraint.
          * @return FilterResults which contains the different results of the serach
          */
         @Override
@@ -130,6 +179,7 @@ public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
                 filterResults.values = suggestions;
                 filterResults.count = suggestions.size();
                 return filterResults;
+
             } else {
                 return new FilterResults();
             }
@@ -138,7 +188,7 @@ public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
 
         /**
          * Method to publish the filtered results of the search on the Arraylist of PriceFinder objects
-         * @param constraint
+         * @param constraint - the item constraint.
          * @param results of the filtered search
          */
         @Override
@@ -158,7 +208,6 @@ public class PriceFinderAdapter extends ArrayAdapter<PriceFinder> {
 
         }
     };
-
 
     public int getSize() {
         return items.size();
